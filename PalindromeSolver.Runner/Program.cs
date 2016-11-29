f13ea964e00cdcc2f5321da6e5675401857bc76c
Graham Moore <mail@grahammoore.co.uk>
2016-11-29 22:19:19 +0000

    Initial commit

---
+++ PalindromeSolver.Runner/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace PalindromeSolver.Runner
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ PalindromeSolver.Tests/SolverTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PalindromeSolver.Tests
+{
+    [TestClass]
+    public class SolverTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
